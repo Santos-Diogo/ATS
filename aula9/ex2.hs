ea00953cab1485fd82115029f2254550e830667d
Samba@X13 <santos.diogo.21@outlook.com>
2024-04-10 13:23:32 +0100

    Meio cagations

--- aula9/ex2.hs
+++ aula9/ex2.hs
@@ -11,7 +11,7 @@ genBST = do
   return (fromList (sort listaInts))
 
 instance Arbitrary BST where
-  arbitrary= genBST
+  arbitrary= genBSTValidOrd
 
 prop_isValidBST :: BST -> Bool
 prop_isValidBST b = isBST b
@@ -34,3 +34,30 @@ isBST (Node l x r) = isBST l && isBST r
         maybeMin Empty = Nothing
         maybeMin (Node Empty x _) = Just x
         maybeMin (Node l _ _) = maybeMin l
+
+balanced :: BST -> Bool
+balanced Empty = True 
+balanced (Node b1 n b2)= abs (heightBST(b1)- heightBST(b2))< 2 && balanced b1 && balanced b2
+
+heightBST :: BST-> Int
+heightBST Empty= 0
+heightBST (Node b1 n b2)= 1+ max (heightBST b1) (heightBST b2)
+
+prop_isBalancedBST:: BST -> Bool
+prop_isBalancedBST b = balanced b
+
+fromListBalanced :: [Int] -> BST
+fromListBalanced [] = Empty
+fromListBalanced l =
+  let size= length l
+      half= div size 2
+      firstHalf= take half l
+      secondHalf= drop half l
+      thisNode= head secondHalf
+      right= tail secondHalf
+  in Node (fromListBalanced firstHalf) thisNode (fromListBalanced right)
+ 
+genBSTValidOrd:: Gen BST
+genBSTValidOrd = do
+  listInt<- listOf(choose(1,1000))
+  return (fromList(sort listInt))
